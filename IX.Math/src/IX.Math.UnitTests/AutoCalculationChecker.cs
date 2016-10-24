c4dbcc86a715c28b85085bf7667d7e11f6b2bb74
adrian.mos <adrian.mos.bp@ebsromania.ro>
2016-10-24 16:49:01 +0300

    Added further tests for expression calculation.

---
+++ IX.Math/src/IX.Math.UnitTests/AutoCalculationChecker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Xunit;
+
+namespace IX.Math.UnitTests
+{
+    public class AutoCalculationChecker
+    {
+        [Theory(DisplayName = "Expression")]
+        [MemberData(nameof(ProvideDataForTheory))]
+        public void AutoCalculationCheckerTest(string expression, object expectedResult)
+        {
+            ExpressionParsingService service = new ExpressionParsingService();
+
+            object result;
+
+            try
+            {
+                result = service.ExecuteExpression(expression);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"The generation process should not have thrown an exception, but it threw {ex.GetType()} with message \"{ex.Message}\".");
+            }
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static object[][] ProvideDataForTheory()
+        {
+            return new object[][]
+            {
+                new object[]
+                {
+                    "3+6",
+                    9
+                },
+                new object[]
+                {
+                    "21*3-17",
+                    46
+                },
+                new object[]
+                {
+                    "(1+1)*2-3",
+                    1
+                },
+                new object[]
+                {
+                    "sqrt(4)",
+                    2.0
+                },
+                new object[]
+                {
+                    "!4+4",
+                    -1
+                },
+            };
+        }
+    }
+}
