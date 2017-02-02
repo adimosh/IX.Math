ede891168c126a37c8c1d0b273fb5931a4427caa
adimosh <adimosh@live.com>
2017-02-02 17:42:31 +0200

    Removed some classes that are no longer in use.

--- IX.Math/ExpressionGenerator.cs
+++ IX.Math/ExpressionGenerator.cs
@@ -6,7 +6,6 @@ using System;
 using System.Linq;
 using System.Text.RegularExpressions;
 using IX.Math.BuiltIn;
-using IX.Math.BuiltIn.Constants;
 using IX.Math.Extraction;
 using IX.Math.Generators;
 using IX.Math.PlatformMitigation;
