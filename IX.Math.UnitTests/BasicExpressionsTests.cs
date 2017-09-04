4b82e4a325ef03c6077e39b94ce07085895a0955
adimosh <adimosh@live.com>
2017-09-04 14:53:15 +0300

    Packages update and unit tests with random data added.

---
+++ IX.Math.UnitTests/BasicExpressionsTests.cs
@@ -0,0 +1,134 @@
+﻿using IX.StandardExtensions.TestUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IX.Math.UnitTests
+{
+    public class BasicExpressionsTests
+    {
+        public static object[][] ProvideDataForTheory()
+        {
+            var tests = new List<object[]>();
+
+            // +
+            {
+                double leftOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue / 2);
+                double rightOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue / 2);
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}+{rightOperand}",
+                        new object[0],
+                        (long)(leftOperand + rightOperand)
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        "leftOperand+rightOperand",
+                        new object[2] { leftOperand, rightOperand },
+                        leftOperand + rightOperand
+                    });
+            }
+
+            // -
+            {
+                double leftOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue);
+                double rightOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue);
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}-{rightOperand}",
+                        new object[0],
+                        (long)(leftOperand - rightOperand)
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        "leftOperand-rightOperand",
+                        new object[2] { leftOperand, rightOperand },
+                        leftOperand - rightOperand
+                    });
+            }
+
+            // *
+            {
+                double leftOperand = DataGenerator.RandomNonNegativeInteger((int)System.Math.Sqrt(int.MaxValue));
+                double rightOperand = DataGenerator.RandomNonNegativeInteger((int)System.Math.Sqrt(int.MaxValue));
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}*{rightOperand}",
+                        new object[0],
+                        (long)(leftOperand * rightOperand)
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        "leftOperand*rightOperand",
+                        new object[2] { leftOperand, rightOperand },
+                        leftOperand * rightOperand
+                    });
+            }
+
+            // /
+            {
+                double leftOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue);
+                double rightOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue);
+                var result = leftOperand / rightOperand;
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}/{rightOperand}",
+                        new object[0],
+                        result
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        "leftOperand/rightOperand",
+                        new object[2] { leftOperand, rightOperand },
+                        result
+                    });
+            }
+
+            return tests.ToArray();
+        }
+
+        [Theory(DisplayName = "Basic expressions with random data")]
+        [MemberData(nameof(ProvideDataForTheory), DisableDiscoveryEnumeration = true)]
+        public void ComputedExpressionWithParameters(string expression, object[] parameters, object expectedResult)
+        {
+            using (var service = new ExpressionParsingService())
+            {
+                ComputedExpression del;
+                try
+                {
+                    del = service.Interpret(expression);
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidOperationException($"The generation process should not have thrown an exception, but it threw {ex.GetType()} with message \"{ex.Message}\".");
+                }
+
+                if (del == null)
+                {
+                    throw new InvalidOperationException("No computed expression was generated!");
+                }
+
+                object result;
+                try
+                {
+                    result = del.Compute(parameters);
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidOperationException($"The method should not have thrown an exception, but it threw {ex.GetType()} with message \"{ex.Message}\".");
+                }
+
+                Assert.Equal(expectedResult, result);
+            }
+        }
+    }
+}
