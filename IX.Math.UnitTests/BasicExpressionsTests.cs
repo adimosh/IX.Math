a5778d304e0148675504dd36d81d8755e461fd01
adimosh <adimosh@live.com>
2017-11-06 17:33:07 +0200

    Extra tests - bug revealed when having a negative number subtracted.

--- IX.Math.UnitTests/BasicExpressionsTests.cs
+++ IX.Math.UnitTests/BasicExpressionsTests.cs
@@ -11,40 +11,42 @@ namespace IX.Math.UnitTests
         {
             var tests = new List<object[]>();
 
-            // Positive integers
+            #region STAGE 1: Positive integers
 
             // +
             {
                 var limit = int.MaxValue / 2;
                 double leftOperand = DataGenerator.RandomNonNegativeInteger(limit);
                 double rightOperand = DataGenerator.RandomNonNegativeInteger(limit);
+                var expectedResult = leftOperand + rightOperand;
+
                 tests.Add(
                     new object[]
                     {
                         $"{leftOperand}+{rightOperand}",
                         new object[0],
-                        (long)(leftOperand + rightOperand)
+                        (long)expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         "leftOperand+rightOperand",
                         new object[2] { leftOperand, rightOperand },
-                        leftOperand + rightOperand
+                        expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         $"leftOperand+{rightOperand}",
                         new object[1] { leftOperand },
-                        leftOperand + rightOperand
+                        expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         $"{leftOperand}+rightOperand",
                         new object[1] { rightOperand },
-                        leftOperand + rightOperand
+                        expectedResult
                     });
             }
 
@@ -52,33 +54,35 @@ namespace IX.Math.UnitTests
             {
                 double leftOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue);
                 double rightOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue);
+                var expectedResult = leftOperand - rightOperand;
+
                 tests.Add(
                     new object[]
                     {
                         $"{leftOperand}-{rightOperand}",
                         new object[0],
-                        (long)(leftOperand - rightOperand)
+                        (long)expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         "leftOperand-rightOperand",
                         new object[2] { leftOperand, rightOperand },
-                        leftOperand - rightOperand
+                        expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         $"{leftOperand}-rightOperand",
                         new object[1] { rightOperand },
-                        leftOperand - rightOperand
+                        expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         $"leftOperand-{rightOperand}",
                         new object[1] { leftOperand },
-                        leftOperand - rightOperand
+                        expectedResult
                     });
             }
 
@@ -87,33 +91,35 @@ namespace IX.Math.UnitTests
                 var limit = (int)System.Math.Sqrt(int.MaxValue);
                 double leftOperand = DataGenerator.RandomNonNegativeInteger(limit);
                 double rightOperand = DataGenerator.RandomNonNegativeInteger(limit);
+                var expectedResult = leftOperand * rightOperand;
+
                 tests.Add(
                     new object[]
                     {
                         $"{leftOperand}*{rightOperand}",
                         new object[0],
-                        (long)(leftOperand * rightOperand)
+                        (long)expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         "leftOperand*rightOperand",
                         new object[2] { leftOperand, rightOperand },
-                        leftOperand * rightOperand
+                        expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         $"{leftOperand}*rightOperand",
                         new object[1] { rightOperand },
-                        leftOperand * rightOperand
+                        expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         $"leftOperand*{rightOperand}",
                         new object[1] { leftOperand },
-                        leftOperand * rightOperand
+                        expectedResult
                     });
             }
 
@@ -121,36 +127,187 @@ namespace IX.Math.UnitTests
             {
                 double leftOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue);
                 double rightOperand = DataGenerator.RandomNonNegativeInteger(int.MaxValue);
-                var result = leftOperand / rightOperand;
+                var expectedResult = leftOperand / rightOperand;
+
                 tests.Add(
                     new object[]
                     {
                         $"{leftOperand}/{rightOperand}",
                         new object[0],
-                        result
+                        expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         "leftOperand/rightOperand",
                         new object[2] { leftOperand, rightOperand },
-                        result
+                        expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         $"leftOperand/{rightOperand}",
                         new object[1] { leftOperand },
-                        result
+                        expectedResult
                     });
                 tests.Add(
                     new object[]
                     {
                         $"{leftOperand}/rightOperand",
                         new object[1] { rightOperand },
-                        result
+                        expectedResult
                     });
             }
+            #endregion
+
+            #region STAGE 1: Negative integers
+
+            // +
+            {
+                var limit = int.MinValue / 2;
+                double leftOperand = DataGenerator.RandomInteger(limit, 1);
+                double rightOperand = DataGenerator.RandomInteger(limit, 1);
+                var expectedResult = leftOperand + rightOperand;
+
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}+{rightOperand}",
+                        new object[0],
+                        (long)expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        "leftOperand+rightOperand",
+                        new object[2] { leftOperand, rightOperand },
+                        expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        $"leftOperand+{rightOperand}",
+                        new object[1] { leftOperand },
+                        expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}+rightOperand",
+                        new object[1] { rightOperand },
+                        expectedResult
+                    });
+            }
+
+            // -
+            {
+                double leftOperand = DataGenerator.RandomInteger(int.MinValue, 1);
+                double rightOperand = DataGenerator.RandomInteger(int.MinValue, 1);
+                var expectedResult = leftOperand - rightOperand;
+
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}-{rightOperand}",
+                        new object[0],
+                        (long)expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        "leftOperand-rightOperand",
+                        new object[2] { leftOperand, rightOperand },
+                        expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}-rightOperand",
+                        new object[1] { rightOperand },
+                        expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        $"leftOperand-{rightOperand}",
+                        new object[1] { leftOperand },
+                        expectedResult
+                    });
+            }
+
+            // *
+            {
+                var limit = 0-(int)System.Math.Sqrt(int.MaxValue);
+                double leftOperand = DataGenerator.RandomInteger(limit, 1);
+                double rightOperand = DataGenerator.RandomInteger(limit, 1);
+                var expectedResult = leftOperand * rightOperand;
+
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}*{rightOperand}",
+                        new object[0],
+                        (long)expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        "leftOperand*rightOperand",
+                        new object[2] { leftOperand, rightOperand },
+                        expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}*rightOperand",
+                        new object[1] { rightOperand },
+                        expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        $"leftOperand*{rightOperand}",
+                        new object[1] { leftOperand },
+                        expectedResult
+                    });
+            }
+
+            // /
+            {
+                double leftOperand = DataGenerator.RandomInteger(int.MinValue, 1);
+                double rightOperand = DataGenerator.RandomInteger(int.MinValue, 1);
+                var expectedResult = leftOperand / rightOperand;
+
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}/{rightOperand}",
+                        new object[0],
+                        expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        "leftOperand/rightOperand",
+                        new object[2] { leftOperand, rightOperand },
+                        expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        $"leftOperand/{rightOperand}",
+                        new object[1] { leftOperand },
+                        expectedResult
+                    });
+                tests.Add(
+                    new object[]
+                    {
+                        $"{leftOperand}/rightOperand",
+                        new object[1] { rightOperand },
+                        expectedResult
+                    });
+            }
+            #endregion
 
             return tests.ToArray();
         }
