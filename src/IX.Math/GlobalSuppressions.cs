04a1d366fa41c1c15c4ffd8457525ab3a51dae8c
Adrian Mos <adrian.mos.bp@nttdata.ro>
2019-09-19 13:29:51 +0300

    Aides to help build not die on too many dire warnings

---
+++ src/IX.Math/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+// <copyright file="HeapAllocationAnalyzerSuppressions.cs" company="Adrian Mos">
+// Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1123:Do not place regions within elements", Justification = "Heeeeeeey! Leave my regions alone! They make complex code breathable!", Scope = "module")]
