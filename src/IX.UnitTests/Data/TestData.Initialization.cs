7f163f11803d8ecdef0a90aee812bcc0bbd04f2d
Adrian Mos <adrian.mos.bp@nttdata.ro>
2020-09-15 11:57:37 +0300

    More formatting of test data initialization.

--- src/IX.UnitTests/Data/TestData.Initialization.cs
+++ src/IX.UnitTests/Data/TestData.Initialization.cs
@@ -13,92 +13,112 @@ namespace IX.UnitTests.Data
     {
         private const int Limit = 1000;
 
-        private static readonly Dictionary<string, (bool, Delegate)> Operators = new Dictionary<string, (bool, Delegate)>
-        {
-            ["+"] = (false, (Func<long, long, long>)((
-                p,
-                q) => p + q)),
-            ["-"] = (false, (Func<long, long, long>)((
-                p,
-                q) => p - q)),
-            ["*"] = (false, (Func<long, long, long>)((
-                p,
-                q) => p * q)),
-            ["/"] = (true, (Func<double, double, double>)((
-                p,
-                q) => p / q)),
-            ["&"] = (false, (Func<long, long, long>)((
-                p,
-                q) => p & q)),
-            ["|"] = (false, (Func<long, long, long>)((
-                p,
-                q) => p | q)),
-            ["^"] = (false, (Func<long, long, long>)((
-                p,
-                q) => p ^ q)),
-        };
+        private static readonly Dictionary<string, (bool, Delegate)> Operators =
+            new Dictionary<string, (bool, Delegate)>
+            {
+                ["+"] = (false, (Func<long, long, long>)((
+                    p,
+                    q) => p + q)),
+                ["-"] = (false, (Func<long, long, long>)((
+                    p,
+                    q) => p - q)),
+                ["*"] = (false, (Func<long, long, long>)((
+                    p,
+                    q) => p * q)),
+                ["/"] = (true, (Func<double, double, double>)((
+                    p,
+                    q) => p / q)),
+                ["&"] = (false, (Func<long, long, long>)((
+                    p,
+                    q) => p & q)),
+                ["|"] = (false, (Func<long, long, long>)((
+                    p,
+                    q) => p | q)),
+                ["^"] = (false, (Func<long, long, long>)((
+                    p,
+                    q) => p ^ q))
+            };
 
-        private static IEnumerable<(object ExpectedResult, string Expression, Dictionary<string, object> ExternalParameters)> GenerateThreeFoldTestData(Dictionary<string, (bool Numeric, Delegate Function)> operators)
+        private static
+            IEnumerable<(object ExpectedResult, string Expression, Dictionary<string, object> ExternalParameters)>
+            GenerateThreeFoldTestData(Dictionary<string, (bool Numeric, Delegate Function)> operators)
         {
-            List<(object, string, Dictionary<string, object>, bool)> initialStage = new List<(object, string, Dictionary<string, object>, bool)>();
+            var initialStage = new List<(object, string, Dictionary<string, object>, bool)>();
             foreach (var op in operators.Keys)
             {
                 var operand1 = DataGenerator.RandomNonNegativeInteger(Limit);
                 var operand2 = DataGenerator.RandomNonNegativeInteger(Limit);
 
                 initialStage.Add(
-                    (
-                        operators[op].Numeric ?
-                            (object)((Func<double, double, double>)operators[op].Function).Invoke(operand1, operand2) :
-                            (object)((Func<long, long, long>)operators[op].Function).Invoke(operand1, operand2),
-                        $"{operand1} {op} {operand2}",
-                        null,
-                        operators[op].Numeric));
+                    (operators[op]
+                        .Numeric
+                        ? (object)((Func<double, double, double>)operators[op]
+                            .Function).Invoke(
+                            operand1,
+                            operand2)
+                        : (object)((Func<long, long, long>)operators[op]
+                            .Function).Invoke(
+                            operand1,
+                            operand2), $"{operand1} {op} {operand2}", null, operators[op]
+                        .Numeric));
                 initialStage.Add(
-                    (
-                        operators[op].Numeric ?
-                            (object)((Func<double, double, double>)operators[op].Function).Invoke(operand1, operand2) :
-                            (object)((Func<long, long, long>)operators[op].Function).Invoke(operand1, operand2),
-                        $"x {op} {operand2}",
-                        new Dictionary<string, object>
-                        {
-                            ["x"] = operand1,
-                        },
-                        operators[op].Numeric));
+                    (operators[op]
+                        .Numeric
+                        ? (object)((Func<double, double, double>)operators[op]
+                            .Function).Invoke(
+                            operand1,
+                            operand2)
+                        : (object)((Func<long, long, long>)operators[op]
+                            .Function).Invoke(
+                            operand1,
+                            operand2), $"x {op} {operand2}", new Dictionary<string, object>
+                    {
+                        ["x"] = operand1
+                    }, operators[op]
+                        .Numeric));
                 initialStage.Add(
-                    (
-                        operators[op].Numeric ?
-                            (object)((Func<double, double, double>)operators[op].Function).Invoke(operand1, operand2) :
-                            (object)((Func<long, long, long>)operators[op].Function).Invoke(operand1, operand2),
-                        $"{operand1} {op} y",
-                        new Dictionary<string, object>
-                        {
-                            ["y"] = operand2,
-                        },
-                        operators[op].Numeric));
+                    (operators[op]
+                        .Numeric
+                        ? (object)((Func<double, double, double>)operators[op]
+                            .Function).Invoke(
+                            operand1,
+                            operand2)
+                        : (object)((Func<long, long, long>)operators[op]
+                            .Function).Invoke(
+                            operand1,
+                            operand2), $"{operand1} {op} y", new Dictionary<string, object>
+                    {
+                        ["y"] = operand2
+                    }, operators[op]
+                        .Numeric));
                 initialStage.Add(
-                    (
-                        operators[op].Numeric ?
-                            (object)((Func<double, double, double>)operators[op].Function).Invoke(operand1, operand2) :
-                            (object)((Func<long, long, long>)operators[op].Function).Invoke(operand1, operand2),
-                        $"x {op} y",
-                        new Dictionary<string, object>
-                        {
-                            ["x"] = operand1,
-                            ["y"] = operand2,
-                        },
-                        operators[op].Numeric));
+                    (operators[op]
+                        .Numeric
+                        ? (object)((Func<double, double, double>)operators[op]
+                            .Function).Invoke(
+                            operand1,
+                            operand2)
+                        : (object)((Func<long, long, long>)operators[op]
+                            .Function).Invoke(
+                            operand1,
+                            operand2), $"x {op} y", new Dictionary<string, object>
+                    {
+                        ["x"] = operand1,
+                        ["y"] = operand2
+                    }, operators[op]
+                        .Numeric));
             }
 
-            List<(object, string, Dictionary<string, object>)> secondStage =
-                new List<(object, string, Dictionary<string, object>)>();
+            var secondStage = new List<(object, string, Dictionary<string, object>)>();
             foreach (var op in operators.Keys)
             {
-                foreach (var xy in initialStage)
+                foreach ((object, string, Dictionary<string, object>, bool) xy in initialStage)
                 {
                     secondStage.Add((xy.Item1, xy.Item2, xy.Item3));
 
-                    if (xy.Item4 != operators[op].Numeric)
+                    if (xy.Item4 !=
+                        operators[op]
+                            .Numeric)
                     {
                         continue;
                     }
@@ -106,37 +126,77 @@ namespace IX.UnitTests.Data
                     var operand3 = DataGenerator.RandomNonNegativeInteger(Limit);
 
                     secondStage.Add(
-                        (
-                            operators[op].Numeric ?
-                                (object)((Func<double, double, double>)operators[op].Function).Invoke(operand3, (double)xy.Item1) :
-                                (object)((Func<long, long, long>)operators[op].Function).Invoke(operand3, (long)xy.Item1),
-                            $"{operand3} {op} ({xy.Item2})",
-                            xy.Item3));
+                        (operators[op]
+                            .Numeric
+                            ? (object)((Func<double, double, double>)operators[op]
+                                .Function).Invoke(
+                                operand3,
+                                (double)xy.Item1)
+                            : (object)((Func<long, long, long>)operators[op]
+                                .Function).Invoke(
+                                operand3,
+                                (long)xy.Item1), $"{operand3} {op} ({xy.Item2})", xy.Item3));
                     secondStage.Add(
-                        (
-                            operators[op].Numeric ?
-                                (object)((Func<double, double, double>)operators[op].Function).Invoke((double)xy.Item1, operand3) :
-                                (object)((Func<long, long, long>)operators[op].Function).Invoke((long)xy.Item1, operand3),
-                            $"({xy.Item2}) {op} {operand3}",
-                            xy.Item3));
+                        (operators[op]
+                            .Numeric
+                            ? (object)((Func<double, double, double>)operators[op]
+                                .Function).Invoke(
+                                (double)xy.Item1,
+                                operand3)
+                            : (object)((Func<long, long, long>)operators[op]
+                                .Function).Invoke(
+                                (long)xy.Item1,
+                                operand3), $"({xy.Item2}) {op} {operand3}", xy.Item3));
                     secondStage.Add(
-                        (
-                            operators[op].Numeric ?
-                                (object)((Func<double, double, double>)operators[op].Function).Invoke(operand3, (double)xy.Item1) :
-                                (object)((Func<long, long, long>)operators[op].Function).Invoke(operand3, (long)xy.Item1),
-                            $"z {op} ({xy.Item2})",
-                            xy.Item3 == null ?
-                                new Dictionary<string, object> { ["z"] = operand3 } :
-                                xy.Item3.Union(new[] { new KeyValuePair<string, object>("z", operand3) }).ToDictionary(p => p.Key, p => p.Value)));
+                        (operators[op]
+                            .Numeric
+                            ? (object)((Func<double, double, double>)operators[op]
+                                .Function).Invoke(
+                                operand3,
+                                (double)xy.Item1)
+                            : (object)((Func<long, long, long>)operators[op]
+                                .Function).Invoke(
+                                operand3,
+                                (long)xy.Item1), $"z {op} ({xy.Item2})", xy.Item3 == null
+                            ? new Dictionary<string, object>
+                            {
+                                ["z"] = operand3
+                            }
+                            : xy.Item3.Union(
+                                    new[]
+                                    {
+                                        new KeyValuePair<string, object>(
+                                            "z",
+                                            operand3)
+                                    })
+                                .ToDictionary(
+                                    p => p.Key,
+                                    p => p.Value)));
                     secondStage.Add(
-                        (
-                            operators[op].Numeric ?
-                                (object)((Func<double, double, double>)operators[op].Function).Invoke((double)xy.Item1, operand3) :
-                                (object)((Func<long, long, long>)operators[op].Function).Invoke((long)xy.Item1, operand3),
-                            $"({xy.Item2}) {op} z",
-                            xy.Item3 == null ?
-                                new Dictionary<string, object> { ["z"] = operand3 } :
-                                xy.Item3.Union(new[] { new KeyValuePair<string, object>("z", operand3) }).ToDictionary(p => p.Key, p => p.Value)));
+                        (operators[op]
+                            .Numeric
+                            ? (object)((Func<double, double, double>)operators[op]
+                                .Function).Invoke(
+                                (double)xy.Item1,
+                                operand3)
+                            : (object)((Func<long, long, long>)operators[op]
+                                .Function).Invoke(
+                                (long)xy.Item1,
+                                operand3), $"({xy.Item2}) {op} z", xy.Item3 == null
+                            ? new Dictionary<string, object>
+                            {
+                                ["z"] = operand3
+                            }
+                            : xy.Item3.Union(
+                                    new[]
+                                    {
+                                        new KeyValuePair<string, object>(
+                                            "z",
+                                            operand3)
+                                    })
+                                .ToDictionary(
+                                    p => p.Key,
+                                    p => p.Value)));
                 }
             }
 
