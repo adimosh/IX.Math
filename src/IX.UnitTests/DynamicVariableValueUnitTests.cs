8fca7e1e180d4144427d250d89a1618195ba345a
Adrian Mos <adrian.mos.bp@nttdata.ro>
2020-11-17 23:34:54 +0200

    Some extra unit tests for DynamicVariableValue.

--- src/IX.UnitTests/DynamicVariableValueUnitTests.cs
+++ src/IX.UnitTests/DynamicVariableValueUnitTests.cs
@@ -2,27 +2,31 @@
 // Copyright (c) Adrian Mos with all rights reserved. Part of the IX Framework.
 // </copyright>
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using IX.Math;
 using Xunit;
 
 namespace IX.UnitTests
 {
+    /// <summary>
+    /// Unit tests for <see cref="DynamicVariableValue"/>.
+    /// </summary>
     public class DynamicVariableValueUnitTests
     {
+        /// <summary>
+        /// DynamicVariableValue initialization forward.
+        /// </summary>
         [Fact(DisplayName = "DynamicVariableValue initialization forward")]
         public void Test1()
         {
+            // ARRANGE
             MathematicPortfolio p = new MathematicPortfolio();
 
             p.RegisterConversion<long, string>(source => (true, source.ToString()));
 
+            // ACT
             DynamicVariableValue dv = DataGeneration.DataGenerator.RandomInteger();
 
+            // ASSERT
             Assert.True(dv.TryGetInteger(out var l));
             Assert.True(dv.TryGetString(out var s));
             Assert.False(dv.TryGetBinary(out _));
@@ -32,15 +36,21 @@ namespace IX.UnitTests
             Assert.Equal(l, long.Parse(s));
         }
 
+        /// <summary>
+        /// DynamicVariableValue initialization reverse.
+        /// </summary>
         [Fact(DisplayName = "DynamicVariableValue initialization reverse")]
         public void Test2()
         {
+            // ARRANGE
             MathematicPortfolio p = new MathematicPortfolio();
 
             p.RegisterConversion<string, long>(source => (true, long.Parse(source)));
 
+            // ACT
             DynamicVariableValue dv = DataGeneration.DataGenerator.RandomInteger().ToString();
 
+            // ASSERT
             Assert.True(dv.TryGetInteger(out var l));
             Assert.True(dv.TryGetString(out var s));
             Assert.False(dv.TryGetBinary(out _));
@@ -48,6 +58,32 @@ namespace IX.UnitTests
             Assert.False(dv.TryGetNumeric(out _));
 
             Assert.Equal(s, l.ToString());
+        }
+
+        /// <summary>
+        /// DynamicVariableValue with simple operation.
+        /// </summary>
+        [Fact(DisplayName = "DynamicVariableValue with simple operation")]
+        public void Test3()
+        {
+            // ARRANGE
+            MathematicPortfolio p = new MathematicPortfolio();
+
+            p.RegisterConversion<string, long>(source => (true, long.Parse(source)));
+
+            long value1 = DataGeneration.DataGenerator.RandomInteger();
+            long value2 = DataGeneration.DataGenerator.RandomInteger();
+            DynamicVariableValue dv = value1.ToString();
+
+            // ACT
+            var result = p.Solve(
+                $"{value2} + x",
+                dv);
+
+            // ASSERT
+            Assert.Equal(
+                result,
+                value1 + value2);
         }
     }
 }
